eeca984f6fd87c20e1e7252c3f9b34ba7dffb66c
Marcos Guilherme <mguilherme1914@gmail.com>
2026-01-13 18:15:02 -0300

    Segunda sessão

---
+++ 2sessao/ex2.php
@@ -0,0 +1,8 @@
+<?php 
+
+// Exercício sobre comentários em PHP 
+// Autor: Marcos Guilherme de Freitas Costa
+// Data: 27/02/2024
+// Descrição: Este script exibe informações sobre sessões em PHP.
+$nome = "devGuiCosta"
+?>
